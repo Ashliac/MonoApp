b8139fa550ea45ee8b61fb6dc81285f960c78da9
Ashliac <ashliac.aam@gmail.com>
2024-09-13 14:42:53 -0600

    Html

--- src/domain/templates/email.templates.ts
+++ src/domain/templates/email.templates.ts
@@ -10,67 +10,73 @@ export function generateCaseEmailTemplate(name: string, lastname:string, genre: 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Case Details</title>
     <style>
-            body {
-                font-family: Arial, sans-serif;
-                background-color: #f4f4f4;
-                color: #333;
-                margin: 0;
-                padding: 0;
-            }
-            .container {
-                width: 100%;
-                max-width: 600px;
-                margin: 20px auto;
-                background-color: #ffffff;
-                border-radius: 8px;
-                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-                overflow: hidden;
-            }
-            .header {
-                background-color: #007BFF;
-                color: #ffffff;
-                padding: 20px;
-                text-align: center;
-            }
-            .header h1 {
-                margin: 0;
-                font-size: 24px;
-            }
-            .content {
-                padding: 20px;
-            }
-            .content p {
-                margin: 10px 0;
-            }
-            .footer {
-                background-color: #f4f4f4;
-                color: #777;
-                padding: 10px;
-                text-align: center;
-                font-size: 12px;
-            }
-            .map-img{
-                width:100%;
-                height: auto;
-                border-radius: 10px;
-            }
-        </style>
+        body {
+            font-family: 'Arial', sans-serif;
+            background-color: #e0f7fa;
+            color: #0277bd;
+            margin: 0;
+            padding: 0;
+        }
+        .container {
+            width: 100%;
+            max-width: 600px;
+            margin: 20px auto;
+            background-color: #ffffff;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            overflow: hidden;
+        }
+        .header {
+            background-color: #01579b;
+            color: #ffffff;
+            padding: 20px;
+            text-align: center;
+        }
+        .header h1 {
+            margin: 0;
+            font-size: 28px;
+            font-weight: bold;
+        }
+        .header h2 {
+            margin: 10px 0 0;
+            font-size: 20px;
+        }
+        .content {
+            padding: 20px;
+        }
+        .content p {
+            margin: 10px 0;
+        }
+        .footer {
+            background-color: #b3e5fc;
+            color: #01579b;
+            padding: 10px;
+            text-align: center;
+            font-size: 12px;
+        }
+        .map-img {
+            width: 100%;
+            height: auto;
+            border-radius: 10px;
+        }
+    </style>
 </head>
 <body>
     <div class="container">
         <div class="header">
-            <h1>Case Details</h1>
+            <h1>Monkeypox</h1>
+            <h2>Case Details</h2>
         </div>
         <div class="content">
             <div class="detail-group">
-                <h2>Person Information</h2>
+                <h3>Person Information</h3>
                 <p><strong>Name:</strong> ${name}</p>
                 <p><strong>Lastname:</strong> ${lastname}</p>
                 <p><strong>Genre:</strong> ${genre}</p>
                 <p><strong>Age:</strong> ${age}</p>
             </div>
             <div class="detail-group">
-                <h2>Incident Information</h2>
+                <h3>Incident Information</h3>
                 <p><strong>Latitude:</strong> ${lat}</p>
                 <p><strong>Longitude:</strong> ${lng}</p>
             </div>
@@ -78,23 +84,19 @@ export function generateCaseEmailTemplate(name: string, lastname:string, genre: 
         </div>
         <div class="footer">
             <p>This is an automatically generated email. Please do not reply.</p>
-            <p class="footer-title">Monkeypox Incidents</p>
+            <p>Monkeypox Incidents</p>
         </div>
     </div>
 </body>
 </html>
-
-
-    
     `;
 }
 
+export const generateMapboxStaticImageURL = (lat: number, lng: number) => {
+    const accessToken = envs.MAPBOX_ACCESS_TOKEN; // Replace with your Mapbox access token
+    const zoom = 15; // Zoom level
+    const width = 800; // Image width
+    const height = 500; // Image height
 
-export const generateMapboxStaticImageURL= (lat:number, lng:number) =>{
-    const accessToken = envs.MAPBOX_ACCESS_TOKEN; // Reemplaza con tu token de acceso de Mapbox
-    const zoom = 15; // Nivel de zoom
-    const width = 800; // Ancho de la imagen
-    const height = 500; // Altura de la imagen
- 
     return `https://api.mapbox.com/styles/v1/mapbox/light-v11/static/pin-l-embassy+f74e4e(${lng},${lat})/${lng},${lat},${zoom}/${width}x${height}?access_token=${accessToken}`;
 }
